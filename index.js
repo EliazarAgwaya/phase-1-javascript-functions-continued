77c45e71f7c6006d3d31e54a50cd3fe2879c8fcd
Eliazar Agwaya <eliazar.magero@student.moringaschool.com>
2022-06-07 09:04:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun("code");
+
+const mondayWork=function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("be indoors");
+function wrapAdjective(symbol="*"){
+    return function(book="special"){
+        return `You are ${symbol}${book}${symbol}!`;
+    }
+}
+wrapAdjective();
